c56cdedad9ed99ff8e69b3e7b8c1b291c74b0515
Alice <aywang6@gmail.com>
2020-08-30 19:15:23 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,25 +1,33 @@
-/*
-  Given that you have an already valid JSON string where the keys are
-  all kebab-case, write a function called transformKebabCaseToCamelCase 
-  that takes in this JSON string and returns a JSON string where all
-  the keys are transformed to be camelCase.
+function transformKebabCaseToCamelCase(data) {
+  const transformedObject = JSON.parse(data, reviver);
+  return JSON.stringify(transformedObject);
+}
 
-  Turn a JSON string to an object
-    JSON.parse(data: string): object
+function reviver(key, value) {
+  const object = {};
 
-  Turn an object to a JSON string
-    JSON.stringify(data: object): string
+  if (key) {
+    key = toCamel(key);
+    console.log(key, value);
+    object[key] = value;
+    return object;
+  }
+}
 
-  Get the keys of an object into an array
-    Object.keys(data: object): Array(string)
-  
-  Iterate through each key of an object.
-    for (const property in object) {
-      console.log(`${property}: ${object[property]}`);
+function toCamel(s) {
+  let camelCased = s[0];
+
+  for (let i = 1; i < s.length; i++) {
+    if (s[i - 1] === "-") {
+      camelCased = camelCased + s[i].toUpperCase();
+    } else if (s[i] !== "-") {
+      camelCased = camelCased + s[i];
     }
-*/
+  }
 
-//Advanced example is an array
+  return camelCased;
+}
+
 const exampleJson = `{
   "first-name-and-middle-initial": "Jeff D.",
   "last-name": "Green",
@@ -27,63 +35,11 @@ const exampleJson = `{
     "city-and-state": "Boston, MA",
     "zip": "12345-1234"
   },
-  "pets": [{
+  "pets": {
     "pet-name": "Bruno",
     "pet-gender": "male"
-  }]
+  }
 }`;
 
-// Get the keys from the JSON that was passed in
-// Look through each letter of the key
-// remove the dashes
-// leave first word's first letter as lower case
-// change other words' first letters to uppercase
-// return updated key
-// Update the JSON keys using the updated strings
-// Return the updated JSON
-
-// Deprecated
-// break key into words "first-name" ==> "first" "name" key.split('-');
-
-function transformKebabCaseToCamelCase(data) {
-  const object = JSON.parse(data);
-  const keys = Object.keys(object);
-
-  const modifiedKeys = keys.map((key) => {
-    let modifiedKey = "";
-    for (let letter = 0; letter < key.length; letter++) {
-      // key[i] !== -
-      if (key[letter] === "-") {
-        continue;
-      }
-
-      if (key[letter - 1] === "-") {
-        modifiedKey = `${modifiedKey}${key[letter].toUpperCase()}`;
-      } else {
-        modifiedKey = `${modifiedKey}${key[letter]}`;
-      }
-    }
-
-    return modifiedKey;
-  });
-
-  let x = 0;
-
-  for (const property in object) {
-    object[modifiedKeys[x]] = object[property];
-
-    if (!modifiedKeys.includes(property)) {
-      delete object[property];
-    }
-
-    x++;
-  }
-
-  const kebabCased = JSON.stringify(object);
-
-  return kebabCased;
-}
-
 const result = transformKebabCaseToCamelCase(exampleJson);
-// console.log(result);
 document.getElementById("app").innerHTML = result;
